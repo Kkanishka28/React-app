ca3b03b38d2d44926ab039acd3fbc18c97d089ba
Kkanishka28 <Kkanishka28@gmail.com>
2022-04-18 19:11:42 +0530

    Initial commit

--- src/Effect.js
+++ src/Effect.js
@@ -1,5 +1,6 @@
 import {useEffect, useState} from "react";
 import Propseffect from "./Propseffect";
+
 function Effect(){
 const[data,setData]=useState(10)
 const[count,setCount]=useState(100)
@@ -8,14 +9,15 @@ const[count,setCount]=useState(100)
     //     console.log("useeffect called")
     // },[data])
     return(
-<div>
+<>
 
   <Propseffect  data={data} count={count}/>
   
 
 <button onClick={()=>setCount(count+1)}>update count</button>
 <button onClick={()=>setData(data+1)}>update data</button>
-</div>
+</>
+
     )
 }
 export default Effect;
